cb59a0ed2b128c1579b6c188091da17a65fa4103
Pablo <BenzoFuryWolf pzarov0@gmail.com>
2024-06-07 13:55:09 +0300

    My

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,9 @@
 
 import './App.css'
-import {useSelector,useDispatch} from "react-redux";
+import Bank from "./components/bank.jsx";
 
 function App() {
-    const dispatch = useDispatch()
-    const cash = useSelector(state => state.cash)
-    const nominal = {
-        "5":5,
-        "10":10,
-        "50":50,
-        "100":100
-    }
-
-    const AddState = (payload) =>{
-        dispatch({type:"ADD_STATE", payload:payload})
-    }
-    const SubtractState = (payload) =>{
-        dispatch({type:"SUBTRACT_STATE", payload:payload})
-    }
-
-  return (
-    <>
-      <div>
-
-      </div>
-      <h1>MyDispatcher</h1>
-        <div className="card">
-            <h1>
-                {cash}
-            </h1>
-            <div>
-                <button onClick={() => AddState(nominal["5"])}>
-                    Add 5
-                </button>
-                <button onClick={() => AddState(nominal["10"])}>
-                    Add 10
-                </button>
-                <button onClick={() => AddState(nominal["50"])}>
-                    Add 50
-                </button>
-                <button onClick={() => AddState(nominal["100"])}>
-                    Add 100
-                </button>
-            </div>
-            <div>
-                <button onClick={() => SubtractState(nominal["5"])}>
-                    Subtract 5
-                </button>
-                <button onClick={() => SubtractState(nominal["10"])}>
-                    Subtract 10
-                </button>
-                <button onClick={() => SubtractState(nominal["50"])}>
-                    Subtract 50
-                </button>
-                <button onClick={() => SubtractState(nominal["100"])}>
-                    Subtract 100
-                </button>
-            </div>
-
-        </div>
-    </>
-  )
+        return(<Bank></Bank>)
 }
 
 
